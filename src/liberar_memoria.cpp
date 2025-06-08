493f8b3f7fc4f190e0c42275f1941e5fab3db03c
Fabricio Valencia <72322695@continental.edu.pe>
2025-06-08 11:12:06 -0500

    Create liberar_memoria.cpp
    Se agregó la función liberarMemoria(): realiza un pop en la pila de memoria simulada para liberar el último bloque asignado.

---
+++ src/liberar_memoria.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// CASO:
+// Cuando un proceso termina, libera su memoria.
+// Este archivo quita el bloque más reciente de la pila (pop).
+
+void liberarMemoria() {
+    // Quitar el nodo de la cima de la pila (si existe).
+}
