1549660d3dcc50c553441e626077125ed5bdde50
Fabricio Valencia <72322695@continental.edu.pe>
2025-06-08 11:06:16 -0500

    Create crear_proceso.cpp
    #include <iostream>
    using namespace std;
    
    // CASO:
    // Una empresa quiere registrar procesos con ID, nombre, prioridad y duración.
    // Este archivo se encarga de agregar esos procesos a una lista de espera (estado "Listo").
    
    void crearProceso() {
        // Aquí va tu código para pedir datos y agregarlos a la lista enlazada.
    }

---
+++ src/crear_proceso.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// CASO:
+// Una empresa quiere registrar procesos con ID, nombre, prioridad y duración.
+// Este archivo se encarga de agregar esos procesos a una lista de espera (estado "Listo").
+
+void crearProceso() {
+    // Aquí va tu código para pedir datos y agregarlos a la lista enlazada.
+}
