34b56f408ee6b836314bde814cd4c563deb09f64
Fabricio Valencia <72322695@continental.edu.pe>
2025-06-08 11:07:46 -0500

    Create ver_procesos.cpp
    Se añadió la función verProcesosListo(): muestra todos los procesos actualmente almacenados en la lista de estado "Listo".

---
+++ src/ver_procesos.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// CASO:
+// El jefe quiere revisar todos los procesos en espera.
+// Este archivo muestra todos los procesos en la lista "Listo".
+
+void verProcesosListo() {
+    // Aquí recorres y muestras la lista enlazada con todos los procesos.
+}
