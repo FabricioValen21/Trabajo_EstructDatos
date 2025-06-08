e9bf7529c2fab17e1f5a7d09e0caa0a5a092f93e
Fabricio Valencia <72322695@continental.edu.pe>
2025-06-08 11:52:18 -0500

    Update ver_procesos.cpp

--- src/ver_procesos.cpp
+++ src/ver_procesos.cpp
@@ -42,6 +42,7 @@ extern ProcesoBloqueado* colaBloqueados;
 // El jefe quiere revisar todos los procesos en espera.
 // Este archivo muestra todos los procesos en la lista "Listo".
 
+
 void verProcesosListo() {
     // Aquí recorres y muestras la lista enlazada con todos los procesos.
 }
