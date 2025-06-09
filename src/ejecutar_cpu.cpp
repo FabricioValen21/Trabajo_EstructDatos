b7aa44b5d9bb4d4706be59b69392605e1b5c44ed
juancarloscuti <74426510@continental.edu.pe>
2025-06-09 09:14:12 -0500

    juan carlos cuti Muñoz
    update od code

--- src/ejecutar_cpu.cpp
+++ src/ejecutar_cpu.cpp
@@ -6,4 +6,26 @@ using namespace std;
 // Este archivo desencola un proceso, reduce su duración y decide si terminó o vuelve a la cola.
 
 void ejecutarProceso() {
+   void ejecutarProceso() {
+    if (colaCPU == nullptr) {
+        cout << "No hay procesos para ejecutar\n";
+        return;
+    }
+
+    ProcesoCPU* proceso = desencolar();
+    proceso->duracion--;
+
+    cout << "Ejecutando proceso: " << proceso->nombre 
+         << " (ID: " << proceso->id << "), Duraci�n restante: " << proceso->duracion << endl;
+
+    if (proceso->duracion <= 0) {
+        cout << "Proceso finalizado.\n";
+        liberarMemoria(proceso->id);
+        delete proceso;
+    } else {
+        encolar(proceso);
+    }
 }
+
+}
+
