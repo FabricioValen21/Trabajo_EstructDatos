c8ebde79efc5cde3fffcfd07fa87cb121e53d4be
Fabricio Valencia <72322695@continental.edu.pe>
2025-06-09 05:02:11 -0500

    Avance cambiar_estado.cpp
    Se realizo un avance en cambiar_estado.cpp, ya esta listo el mover procesos prioridad (2,1,0)

--- src/cambiar_estado.cpp
+++ src/cambiar_estado.cpp
@@ -38,10 +38,74 @@ extern ProcesoBloqueado* colaBloqueados;
 
 // ==== FIN DE VARIABLES ====
 
-// CASO:
-// Un proceso puede cambiar de estado (por ejemplo: de "Listo" a "CPU").
-// Este archivo busca un proceso y lo mueve a la estructura correcta segÃºn su nuevo estado.
+// cambiar_estado.cpp
+// Implementa la funcionalidad del menú: "3. Cambiar estado de un proceso"
+// Esta función permite buscar un proceso por su ID, eliminarlo de la lista actual e insertarlo en la estructura correspondiente (CPU o bloqueado)
 
 void cambiarEstado() {
-    // Buscar por ID, quitar de la lista actual e insertar en la nueva estructura.
+    // Pedimos el ID del proceso que se desea mover
+    int idBuscar;
+    cout << "Ingrese el ID del proceso que desee mover: ";
+    cin >> idBuscar;
+    
+    // Se busca el proceso en la lista "listaListos"
+    Proceso* actual = listaListos;
+    Proceso* anterior = NULL;
+    
+    // Se recorre la lista para encontrar el proceso del ID ingresado
+    while (actual != NULL && actual->id != idBuscar) {
+    	anterior = actual;
+    	actual = actual->siguiente;
+	}
+	
+	// Verificamos que el proceso no haya sido encontrado
+	if (actual = NULL) {
+		cout << "Proceso no encontrado.\n";
+		return;
+	}
+	
+	// Quita el node de la lista
+	if (anterior == NULL) {
+		// Si el nodo esta al inicio de la lista
+		listaListos = actual->siguiente;
+	} else {
+		// Si el nodo esta en el medio o al final
+		anterior->siguiente = actual->siguiente;
+	}
+	
+	// Ahora el nodo actual esta desconectado de su lista
+	actual->siguiente = NULL;
+	
+	// Se pregunta al usuario que estado desea mover el proceso
+	int opcion;
+	cout << "Seleccioneel nuevo estado del proceso:\n";
+	cout << "1. Mover a el CPU (cola de prioridad)\n";
+	cout << "2. Mover a bloqueados (espera por E/S)\n";
+	cout << "Opcion: ";
+	cin >> opcion;
+	
+	if (opcion == 1) {
+		// Inserta el proceso en la cola de CPU (ordenando por prioridad)
+
+		if(colaCPU == NULL || actual->prioridad > colaCPU->prioridad) {
+			// Si la cola esta vacia o el proceso tiene mayor prioridad, va al inicio
+			actual->siguiente = colaCPU;
+			cola CPU = actual;		
+		} else {
+			// Buscar la posicion correcta segun prioridad
+			Proceso* temp = colaCPU;
+			while (temp->siguiente != NULL && temp->siguiente->prioridad >= actual->prioridad) {
+				temp = temp->siguiente;
+			}
+			
+			// Insertar el proceso en su posicion
+			actual->siguiente = temp->siguiente;
+			temp->siguiente = actual;
+		}
+	
+		cout << "Proceso fue movido a la CPU.\n";
+	} else if (opcion == 2) {
+		
+	}
+	
 }
